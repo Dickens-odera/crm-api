5cc82cacf4b5baa18f26567170c9aa2fa18ecfa5
Dickens-Odera <dickensodera9@gmail.com>
2021-07-22 07:02:27 +0300

    Added heroku app url

--- config/scribe.php
+++ config/scribe.php
@@ -244,7 +244,7 @@ INTRO
     /*
      * The base URL to be used in examples. If this is empty, Scribe will use the value of config('app.url').
      */
-    'base_url' => null,
+    'base_url' => 'https://agile-monkeys-api-backend.herokuapp.com/',
     /*
      * Generate a Postman collection (v2.1.0) in addition to HTML docs.
      * For 'static' docs, the collection will be generated to public/docs/collection.json.
